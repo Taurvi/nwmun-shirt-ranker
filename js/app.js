3ab7e253f59b0e858b2205b4c0764e30e853eec8
Stephen Rimbakusumo <srimbakusumo@gmail.com>
2016-06-06 13:43:24 -0700

    Added prod/dev stuff

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 'use strict';
-var socket = io('http://localhost:3000');
+// PROD
+var socket = io('http://52.41.5.103:3000');
+// DEV
+//var socket = io('http://localhost:3000');
 var ngApp = angular.module('ngApp', ['ui.bootstrap'])
